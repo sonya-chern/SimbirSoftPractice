57baaff74c0ea159971954609ebaae87b982c1ec
Sofia Chernyshova <73364483+sonya-chern@users.noreply.github.com>
2021-04-23 22:14:12 +0400

    Add files via upload

---
+++ BookDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationProject
+{
+    public class BookDto
+    {
+        private string bookTitle;
+        PersonDto authorName;
+        private string booksGenre;
+        public static List<BookDto> listBooks = new List<BookDto>(3);
+    }
+
+    
+}
